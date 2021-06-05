ccc82eac6e5d189e4be34949f5b16b402bbb3aac
Suraj Jha <surajjha039@gmail.com>
2021-06-05 15:10:16 +0530

    qestion route updated

--- router/question.js
+++ router/question.js
@@ -4,11 +4,12 @@ const router = express.Router();
 const Question = require("../models/question");
 
 router.get('/questions/:scode', async (req, res) => {
-    const qbank = await Question.find( { "subjectCode": req.params.scode },{"added":1});
+    res.send("Welcome to questions");
+    /*const qbank = await Question.find( { "subjectCode": req.params.scode },{"added":1});
     for(let q of qbank){
         console.log(q.added);
     }
-    
+    */
 });
 
 module.exports = router;
